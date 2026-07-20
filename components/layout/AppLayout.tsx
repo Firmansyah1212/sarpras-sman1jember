20d850d8b9af9b2ae3ed2b3073c321ca9a778142
Firmansyah Wahyu Maulana <firman12df@gmail.com>
2026-07-20 20:45:27 +0700

    sidebar dan navbar

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -1,36 +1,27 @@
 "use client";
 
-import { ReactNode } from "react";
-
+import { useState } from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
-interface Props {
-  children: ReactNode;
-}
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-export default function AppLayout({ children }: Props) {
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+  const closeSidebar = () => setIsSidebarOpen(false);
+
   return (
-    <div className="min-h-screen bg-slate-100">
+    <div className="flex h-screen overflow-hidden bg-gray-50">
+      {/* Sidebar - desktop selalu tampil, mobile muncul via overlay */}
+      <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
 
-      <div className="flex">
-
-        <Sidebar />
-
-        <div className="flex-1 min-h-screen">
-
-          <Navbar />
-
-          <main className="max-w-7xl mx-auto p-6">
-
-            {children}
-
-          </main>
-
-        </div>
-
+      {/* Main Content */}
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <Navbar onMenuClick={toggleSidebar} />
+        <main className="flex-1 overflow-y-auto p-4 md:p-6">
+          {children}
+        </main>
       </div>
-
     </div>
   );
 }
